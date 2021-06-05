a212ec41181193e197ebdf34d51ac2c6fcf792e1
Ymahlawat <69887149+Ymahlawat@users.noreply.github.com>
2021-06-05 12:05:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,22 @@ var sec = document.getElementById('second');
 
 button.addEventListener("click", function(){
 
+first.value = first.value.toLowerCase();
+second.value = second.value.toLowerCase();
+
     if(fir.value==""||sec.value==""){
         result.innerHTML = "Please add valid name";
         return;
-    }else if(sec.value=="yogesh"){
+    }else if(sec.value=="yogesh"&&first.value=="sonakshi"){
         result.innerHTML = "Your Love is "+100+" % strong";
         return 
+    }else if(first.value=="yogesh"&&second.value=="sonakshi"){
+        result.innerHTML = "Your Love is "+100+" % strong";
+        return 
+    }else if(first.value=="sonakshi"&&second.value!="yogesh"){
+        result.innerHTML = "Your Love is "+Math.round((Math.random()*40))+" % strong";
+        return 
     }
-
     let a = Math.random();
     a = a*100;
     a=Math.round(a);
